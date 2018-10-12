3720091436e78f2d8f949bc152bc904e5b2938e2
Riad Gozim <rgozim1@gmail.com>
2018-10-12 12:11:47 +0100

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const merge = require("webpack-merge");
-const omeroConfig = require("./webpack/webpack.omero");
-const webpackConfig = require("./webpack/webpack.aurelia");
+const webpackOmero = require("./webpack.omero");
+const webpackAurelia = require("./webpack.aurelia");
+
+// module.exports = ({ production, server, extractCss, coverage, analyze, karma } = {}) => {
+//   let ac = aureliaConfig({ production, server, extractCss, coverage, analyze, karma });
+//   return merge(ac, omeroConfig);
+// }
 
 module.exports = ({ production, server, extractCss, coverage, analyze, karma } = {}) => {
-  let config = webpackConfig({ production, server, extractCss, coverage, analyze, karma });
-  return merge.multiple(config, omeroConfig);
+  const aureliaConfig = merge([
+    webpackAurelia({ 
+      production, server, extractCss, coverage, analyze, karma 
+    }),
+  ]);
+
+  const omeroConfig = merge([
+    webpackOmero
+  ]);
+
+  return merge(aureliaConfig, omeroConfig);
 }
