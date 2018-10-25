a8547dea392c979c021811bdc94de4709fe92ad8
Riad Gozim <rgozim1@gmail.com>
2018-10-26 00:28:36 +0100

    5 errors on build

--- aurelia_project/tasks/build-ol3.js
+++ aurelia_project/tasks/build-ol3.js
@@ -3,52 +3,57 @@ import gulp from 'gulp';
 import del from 'del';
 import gccmain from 'google-closure-compiler';
 
-const srcPath = path.resolve(__dirname, 'libs/ol3-viewer/src/ome');
-const outputPath = path.resolve(__dirname, 'libs/ol3-viewer/dist');
 const closureCompiler = gccmain.gulp();
+const outputPath = './libs/ol3-viewer/dist';
+const srcPath =  path.resolve(__dirname, 'libs/ol3-viewer/src/ome');
+const closureUtils = './node_modules/closure-util';
 
-const ol3Srcs = path.resolve(__dirname, 'node_modules/openlayers');
 
-const outputWrapper = `(function (root, factory) {
-            var tmp = null;
-            if(typeof module === 'object' &amp;&amp; module.exports) {
-                root = window;
-                tmp =  factory.call(root);
-                module.exports = tmp;
-            } else {
-                tmp = factory.call(root);
-                if (typeof root.ome !== 'object') root.ome = {};
-                root.ome = tmp;
-            }
-        }(this, function () {
-            var OME = {};
-            if (typeof(this) === 'object' &amp;&amp; typeof(this.ome) === 'object') {
-                OME.ome = this.ome;
-                var ome = this.ome;
-            }
-            %output%
-            return OME.ome;
-        }));`;
+const srcArray = [
+  'libs/ol3-viewer/src/ome/**.js',
+  'node_modules/openlayers/src/**.js',
+  'node_modules/openlayers/build/ol.ext/**.js'
+];
 
 function clearDist() {
   return del([outputPath]);
 }
 
+// function jsCompile() {
+//   closureBuilder.build({
+//     name: 'name',
+//     srcs: glob([
+//       'libs/ol3-viewer/src/ome/**.js',
+//       'node_modules/openlayers/src/**/*.js',
+//     ]),
+//     out: outputPath + '/merged-and-minified.js'
+//   });
+// }
+
 function jsCompile() {
-  return gulp.src(path.join(srcPath, '**/*.js'), ol3Srcs,  {base: './'})
-    .pipe(closureCompiler({
-      compilation_level: 'SIMPLE',
-      warning_level: 'VERBOSE',
-      language_in: 'ECMASCRIPT6_STRICT',
-      language_out: 'ECMASCRIPT5_STRICT',
-      output_wrapper: outputWrapper,
-      js_output_file: 'ol3-viewer.js'
-    },
-    {
-      platform: ['native', 'java', 'javascript']
-    }))
+  return closureCompiler({
+    js: srcArray,
+    compilation_level: 'ADVANCED',
+    warning_level: 'VERBOSE',
+    js_output_file: 'output.min.js'
+  })
+    .src() // needed to force the plugin to run without gulp.src
     .pipe(gulp.dest(outputPath));
 }
+
+// function jsCompile() {
+//   return gulp.src(path.join(srcPath, '**/*.js'), oljs, olsrc, closureUtils, { base: './' })
+//     .pipe(closureCompiler({
+//       compilation_level: 'SIMPLE',
+//       warning_level: 'VERBOSE',
+//       output_wrapper: outputWrapper,
+//       js_output_file: 'ol3-viewer.js'
+//     },
+//       {
+//         platform: ['native', 'java', 'javascript']
+//       }))
+//     .pipe(gulp.dest(outputPath));
+// }
 
 const build = gulp.series(
   clearDist,
